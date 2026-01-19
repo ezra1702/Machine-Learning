f06c89044d0076770db3025f6a956c8dc5cd2db4
Ezra <ezranewbie17@gmail.com>
2026-01-19 17:41:43 +0700

    update

--- objek_oriented_programming/03_static_class_method/static_class_method.py
+++ objek_oriented_programming/03_static_class_method/static_class_method.py
@@ -1,16 +1,20 @@
 import random
 class Amarine:
-    __unit_total = 0
-    __price_total = 0
-    def __init__(self,id, name, type):
+    _unit_total = 0
+    _price_total = 0
+    def __init__(self,id, name, type, price =0):
         self.__id = id
         self.__name = name
         self.__type = type
+        self.price = price
+        Amarine._unit_total += 1
+        
     #getter
-    def getName(self):
-        return self.__name
-    def getType(self):
-        return self.__type
+    def getName(cls):
+        return cls.__name
+    
+    def getType(cls):
+        return cls.__type
     
     #setter
     def setName(self, name):
@@ -18,19 +22,21 @@ class Amarine:
 
     def setType(self, type):
         self.__type = type
-
-    def addPrice(self, price):
-        self.__price_total += price
-    def totalPrice(self):
-        print(f"Total Harga Kapal: Rp. {self.__price_total:,}")
-    def totalUnit(self):
-        Amarine.__unit_total += 1
-        print(f"Total Kapal: {Amarine.__unit_total}")
+    def addPrice(self, price_components):
+        self.price += price_components
+        Amarine._price_total += price_components
+    @staticmethod
+    def totalPrice():
+        print(f"Total Harga Kapal: Rp. {Amarine._price_total:,}")
+    
+    @classmethod
+    def totalUnit(cls):
+        print(f"Total Kapal: {cls._unit_total}")
     def info(self):
         print(f"Nama Kapal: {self.getName()}")
         print(f"Tipe Kapal: {self.getType()}")
         self.totalPrice()
-        print(f"Harga Kapal: Rp. {self.__price_total:,}")
+        print(f"Harga Kapal: Rp. {self.price:,}")
         self.totalUnit()
         print("=================================")
 
@@ -47,4 +53,13 @@ for i in container:
 
 for i in container:
     i.info()
-    
+
+# Testing static method dan class method
+# static method
+amarine2.totalPrice()
+Amarine.totalPrice()
+# class method
+Amarine.totalUnit()
+amarine2.totalUnit()
+
+
---
+++ objek_oriented_programming/05_inheritance/inheritance.py
@@ -0,0 +1,38 @@
+import random
+from colorama import Fore, Back, Style
+
+class Amarine:
+    _unit_total = 0
+    _price_total = 0
+    def __init__(self, id, name, type, price =0):
+        self.__id = id
+        self.__name = name
+        self.__type = type
+        self.__price = price
+    
+    @property
+    def info(self):
+        return Fore.GREEN  + f"=========================\nNama Kapal: {self.__name}\nTipe Kapal: {self.__type}\nHarga Kapal: Rp. {self.__price:,}"
+    
+
+
+class AUV(Amarine):
+    # def __init__(self, id, name, type):
+    #     self.__id = id
+    #     self.__name = name
+    #     self.__type = type
+    #     self.__price = 10000000
+    # # Output Error
+    def __init__(self, id, name, type):
+        # Amarine.__init__(self, id, name, type, price=10000000)
+        super().__init__(id, name, type, price=10000000)
+    
+
+
+amarine1 = AUV(random.randint(1,100), "AMN-01", "Fishing Boat")
+print(amarine1.info)
+print(amarine1.info)
+
+
+
+

942d301e2b0c8f19c2b90fc8abbc04cb232d1a67
Ezra <ezranewbie17@gmail.com>
2026-01-19 18:04:30 +0700

    update

--- objek_oriented_programming/05_inheritance/inheritance.py
+++ objek_oriented_programming/05_inheritance/inheritance.py
@@ -5,10 +5,10 @@ class Amarine:
     _unit_total = 0
     _price_total = 0
     def __init__(self, id, name, type, price =0):
-        self.__id = id
-        self.__name = name
-        self.__type = type
-        self.__price = price
+        self._id = id
+        self._name = name
+        self._type = type
+        self._price = price
     
     @property
     def info(self):
@@ -26,12 +26,17 @@ class AUV(Amarine):
     def __init__(self, id, name, type):
         # Amarine.__init__(self, id, name, type, price=10000000)
         super().__init__(id, name, type, price=10000000)
+        
+    #Override atribut info dari Amarine
+    @property
+    def info(self):
+            return Fore.RED + f"Ini tipe AUV\n=========================\nNama Kapal: {self._name}\nTipe Kapal: {self._type}\nHarga Kapal: Rp. {self._price:,}" 
     
 
 
 amarine1 = AUV(random.randint(1,100), "AMN-01", "Fishing Boat")
 print(amarine1.info)
-print(amarine1.info)
+print(amarine1.__dict__)
 
 
 
